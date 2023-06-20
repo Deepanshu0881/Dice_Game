d98d49c7a6a41af7771fbc5abf0c0f630f61eadb
Deepanshu0881 <100557902+Deepanshu0881@users.noreply.github.com>
2023-06-20 12:11:39 +0530

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1; // it will generate random numbers between 1 to 6
 var randomDiceImage = "dice"+ randomNumber1 + ".png";// dice1.png - dice6.png
-var randomImageSource = "images/"+randomDiceImage;//images/dice1.png - .....
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomImageSource);
+image1.setAttribute("src",randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random()*6)+1; // it will generate random numbers between 1 to 6
 var randomDiceImage2 = "dice"+ randomNumber2 + ".png";// dice1.png - dice6.png
-var randomImageSource2 = "images/"+randomDiceImage2;//images/dice1.png - .....
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomImageSource2);
+image2.setAttribute("src",randomDiceImage2);
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player1 Wins! 🚩";
